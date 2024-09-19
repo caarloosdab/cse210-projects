a87d688f6cd2311873307d353ba09d4070b24c8f
caarloosdab <caarloosdab@gmail.com>
2024-09-18 21:32:29 -0600

    letter for grade and stretch

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+        
+        char secondNumber = percentage[1];
+        int secondDigit = int.Parse(secondNumber.ToString());
+        
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";   
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("Sorry, you failed");
+        }
+
+        string sign = "";
+
+        if (secondDigit >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if (secondDigit < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
     }
 }
