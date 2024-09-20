7113ca91e89cbb15100a3f7b8a616542d5bf2fdf
caarloosdab <caarloosdab@gmail.com>
2024-09-19 21:53:42 -0600

    guessing game with stretches

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+    string again = "yes";
+
+        while (again == "yes")
+        {    
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = 0;
+            int guesses = 0;
+        
+            while (guess != magicNumber)
+            {
+
+                Console.WriteLine("Guess the magic number ");
+                string number = Console.ReadLine();
+                guess = int.Parse(number);
+                
+            
+                if (guess < magicNumber) 
+                {
+                    Console.WriteLine("Too low, guess again");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Too high, guess again");
+                }
+                else 
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                guesses += 1;
+
+            }
+            Console.WriteLine($"You guessed the magic number in {guesses} guesses");
+
+            Console.WriteLine("Do you wanna keep playing? ");
+            again = Console.ReadLine();
+        
+        }
+        
     }
 }
