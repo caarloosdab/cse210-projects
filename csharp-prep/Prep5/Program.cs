31b8611594064d164dde905a43b8b74e6f7236b8
caarloosdab <caarloosdab@gmail.com>
2024-09-21 15:22:04 -0600

    functions practice done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+        
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{name}, the square of you number is {number*number}");
+
     }
 }
