aa371639eae7597552ec70f7fc075b070e4c0d96
caarloosdab <caarloosdab@gmail.com>
2024-09-30 21:36:21 -0600

    Encapsulation done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractiontostring());
+        Console.WriteLine(f1.GetDecimalValue()); 
+
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractiontostring());
+        Console.WriteLine(f2.GetDecimalValue()); 
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractiontostring());
+        Console.WriteLine(f3.GetDecimalValue()); 
+
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractiontostring());
+        Console.WriteLine(f4.GetDecimalValue()); 
+
     }
 }
