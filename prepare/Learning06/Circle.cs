799b4e75467ada6f4753f5997ab262abd0197df8
caarloosdab <caarloosdab@gmail.com>
2024-10-22 21:36:01 -0600

    Polymorphism Learning Activity

---
+++ prepare/Learning06/Circle.cs
@@ -0,0 +1,15 @@
+class Circle : Shape
+{
+    private double _radius;
+    private double _pi = Math.PI;
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * _pi;
+    }
+
+}
--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 4);
+        shapes.Add(s1);
+        Rectangle s2 = new Rectangle("Blue", 7, 3);
+        shapes.Add(s2);
+        Circle s3 = new Circle("Yellow", 4);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"Shape color: {color}, Shape Area: {area}");
+
+        }
     }
+
 }
